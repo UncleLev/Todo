6e01d8f55047a13eec51380c7368e061eb3b9604
UncleLev <risentean@gmail.com>
2019-11-19 16:22:33 +0200

    Updates

---
+++ precache-manifest.a6b4030727ea24a9df8b8e08746c5b52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372348d53d366c4e76903489596d38c2",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "46c55da81016793445a9",
+    "url": "/Todo/static/css/main.238f608a.chunk.css"
+  },
+  {
+    "revision": "88ef087707047244ac20",
+    "url": "/Todo/static/js/2.65dd199b.chunk.js"
+  },
+  {
+    "revision": "46c55da81016793445a9",
+    "url": "/Todo/static/js/main.2714f6f1.chunk.js"
+  },
+  {
+    "revision": "6e1ac461000570272709",
+    "url": "/Todo/static/js/runtime~main.9145d80b.js"
+  }
+]);
